14c9d1680d95bea026838aa98c74c3183a756a06
Rishang <rishangkumar9693@gmail.com>
2023-12-30 21:04:18 +0530

    added nav bar and setup all dependencies

--- assignment/src/Components/Nav.jsx
+++ assignment/src/Components/Nav.jsx
@@ -1,40 +1,42 @@
-import { Outlet } from "react-router-dom"
-import {AiOutlineShoppingCart} from "react-icons/ai" 
-import { Link } from "react-router-dom"
-import { useState } from "react"
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'; 
+import {AiOutlineShoppingCart} from "react-icons/ai";
 
 const Nav = () => { 
-    const [count,setCount]=useState(0)
+     const [open,setOpen]=useState(false);  
+     const [count,setCount] =useState(0)
+
   return (
-    <>  
-      <nav className="p-5 bg-white shadow md:flex md:items-center md:justify-evenly">
-    <div className="flex  items-center ">
-      <span className="text-2xl font-[Poppins] cursor-pointer">
-       Digital Store
-      </span>
+    <div className='shadow-md w-full fixed top-0 left-0'>
+      <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
+      <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800'>
+        <span className='text-3xl text-indigo-600 mr-1 pt-2'>
+        <ion-icon name="logo-ionic"></ion-icon>
+        </span>
+        Designer
+      </div>
+      
+      <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+      <ion-icon name={open ? 'close':'menu'}></ion-icon>
+      </div>
 
-      <span className="text-3xl cursor-pointer mx-2 md:hidden block">
-        <ion-icon name="menu" onclick="Menu(this)"></ion-icon>
-      </span>
-    </div>
-
-    <ul className="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
+      <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
       <li className="mx-4 my-6 md:my-0">
-        <Link href="#" className="text-xl hover:text-cyan-500 duration-500">HOME</Link>
+        <Link to={'/'} className="text-xl hover:text-cyan-500 duration-500">HOME</Link>
       </li>
       <li className="mx-4 my-6 md:my-0">
-        <Link to={"/explore-more"} className="text-xl hover:text-cyan-500 duration-500">Explore More</Link>
-      </li> 
-      <Link to={"/cart"}>
-      <button className="bg-cyan-200 text-white font-[Poppins] duration-500 px-6 py-2 mx-4 hover:bg-cyan-500 rounded ">
-        <AiOutlineShoppingCart size={20}/> 
-      </button> <span className="relative right-[54px] bottom-[14px]">{count}</span>
-      </Link>
-    </ul>
-  </nav>
-
-    <Outlet/>
-    </>
+        <Link to={'/categories'} className="text-xl hover:text-cyan-500 duration-500">Explore-more</Link>
+      </li>
+        <Link to={"/cart"}> 
+        <button className="bg-cyan-200 text-white font-[Poppins] duration-500 px-6 py-2 mx-4 hover:bg-cyan-500 rounded ">
+        <AiOutlineShoppingCart size={25}/>
+      </button>
+        </Link> 
+        <span className='text-2xl relative right-10 bottom-1'>{count}</span>
+      </ul>
+      </div>
+    </div>
   )
 }
 
