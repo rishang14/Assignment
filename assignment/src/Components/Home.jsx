ebad792280327abfd9849132f6df42a1edb35ed7
Rishang <rishangkumar9693@gmail.com>
2023-12-31 15:56:41 +0530

    added card component with add to cart button and styleing

--- assignment/src/Components/Home.jsx
+++ assignment/src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from 'axios'; 
+import CardCompnent from "./CardCompnent";
 
 const Home = () => {
   const [data, setData] = useState([]);  
@@ -45,33 +46,33 @@ const Home = () => {
   return (
     <>  
     <div className="mt-[60px] mx-auto max-w-[75rem] px-0 py-[4rem]"> 
-    <div className="flex justify-center items-center w-full md:w-[40px]">
+    <div className="flex justify-center items-center w-full lg:w-[full] ">
         <input type="text" className="block w-[150%] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6" placeholder="Search Product" onChange={Filter} /> 
     
 </div>
         <div className="flex justify-center flex-row mt-2 mb-3 flex-wrap"> <span className=" px-6 py-2 mx-4">
           Sort via
         </span>
-        <button className=" text-black font-[Poppins] duration-500 px-6 py-2 mx-4 hover:bg-cyan-500 rounded border-black border-2" onClick={FilterViaPrice}>less than 500</button> 
-        <button className=" text-black font-[Poppins] duration-500 px-6 py-2 mx-4 hover:bg-cyan-500 rounded border-black border-2 " onClick={FilterViaPrice}>3 rating </button> 
-        <button className=" text-black font-[Poppins] duration-500 px-6 py-2 mx-4 hover:bg-cyan-500 rounded border-black border-2">more than 1000</button>  
+        <button className=" text-black font-[Poppins] duration-500 px-6 py-2 mx-4 hover:bg-cyan-500 rounded border-cyan-100 border-2 " onClick={FilterViaPrice}>less than 500</button> 
+        <button className=" text-black font-[Poppins] duration-500 px-6 py-2 mx-4 hover:bg-cyan-500 rounded border-cyan-100 border-2 " onClick={FilterViaPrice}>3 rating </button> 
+        <button className=" text-black font-[Poppins] duration-500 px-6 py-2 mx-4 hover:bg-cyan-500 rounded border-cyan-100 border-2 ">more than 1000</button>  
         </div> 
 
  
     <div className="flex flex-row  justify-center flex-wrap  " > 
-        {search.map((item) => (
-          <div key={item.id} className="flex  flex-row flex-wrap w-[350px] h-[350px]  border-black  border-2  m-2 p-2">  
-          <div className=" border-black border-2">
-
-            <img src={item.thumbnail} alt={item.name} className="object-cover w-full h-[250px]"/>
-          </div> 
-          <div className="">
-
-            <p className="p-2 text-l"> {item.title}</p>  
-            <p className="p-2 "> Rs:{item.price}</p> 
-            <p className="p-2">{item.rating}</p>
-          </div>
-          </div>
+        {search.map((item) => (  
+            <CardCompnent  
+            title={item.title} 
+            discount={item.discountPercentage}
+            id={item.id} 
+            price={item.price} 
+            stock={item.stock} 
+            rating={item.rating} 
+            img={item.thumbnail} 
+            brand={item.brand} 
+            description={item.description}
+            
+            />
         ))}
       </div>  
       </div>
