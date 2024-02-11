2fcbcea091c24619b5ef09358fec647d1c685f9b
Rishang <rishangkumar9693@gmail.com>
2024-02-11 18:06:18 +0530

    cart updation solved

--- assignment/src/Components/Cart.jsx
+++ assignment/src/Components/Cart.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { useAuth } from "../Authentication/Authcontext"  
-import { useNavigate } from "react-router-dom";
+import { json, useNavigate } from "react-router-dom";
 
 const Cart = () => { 
-    const {cart,removeFromCart,setCart,user}=useAuth()  
+    const {cart,removeFromCart,setCart,user,setCount}=useAuth()  
     const [quantity,setQuantity]=useState({})
     const navigate=useNavigate()
     const calculateTotal = (cart) => {
@@ -22,7 +22,13 @@ const Cart = () => {
       const handleCheckout=()=>{
         if(cart.length>0){
             alert("your order successed"); 
-            setCart([]);   
+            setCount(0) 
+            setCart(()=> {
+
+             const newCart=[] 
+             localStorage.setItem('cart',JSON.stringify(newCart))
+            }
+            );   
             navigate("/")
 
             
@@ -36,7 +42,7 @@ const Cart = () => {
     <h1 className="mb-6 text-center text-lg md:text-2xl font-bold">Cart Items</h1>
    
     { 
-      user ?  <div className="mx-auto max-w-5xl px-6 md:flex md:space-x-6 xl:px-0">  <div className="rounded-lg md:w-2/3">
+      user  ?  <div className="mx-auto max-w-5xl px-6 md:flex md:space-x-6 xl:px-0">  <div className="rounded-lg md:w-2/3">
       {cart.map((item) => (
         <div key={item.id} className="mb-6 bg-white p-4 md:p-6 shadow-md rounded-lg">
           <img src={item.thumbnail} alt={item.title} className="w-full rounded-lg mb-4 md:w-40 md:mb-0" />
@@ -44,12 +50,12 @@ const Cart = () => {
             <div className="flex items-center flex-wrap space-x-4">
               <p>{item.brand}</p>
               <p>{item.title}</p>
-              <p className="text-sm">Price:{(quantity[item.id] || 1 ) * item.price}</p>
+              <p className="text-sm">Price:{(quantity[item.id] || 1 ) * item.price}</p> 
             </div> 
             <div>
-              <span  className="cursor-pointer" onClick={()=>handleDecrement(item.id)}>⬅️</span>  
+              <span  className="cursor-pointer p-2" onClick={()=>handleDecrement(item.id)}>⬅️</span>  
               {quantity[item.id] || 1}
-              <span className="cursor-pointer" onClick={()=> handleIncrement(item.id)}>➡️</span>
+              <span className="cursor-pointer p-2" onClick={()=> handleIncrement(item.id)}>➡️</span>
             </div>
             <button
               className="mt-2 md:mt-0 rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500"
@@ -72,13 +78,13 @@ const Cart = () => {
           Check out
         </button>
       </div>
-    </div> :   <div className="mt-6 h-full rounded-lg border bg-white p-4 md:p-6 shadow-md md:mt-0 md:w-1/3">
+    </div> :   <div className="mt-6 h-[200px] rounded-lg border bg-white p-4 md:p-6 shadow-md md:mt-0 md:w-1/3">
         <hr className="my-4" />
         <div className="flex justify-between mb-4">
           <p className="text-sm md:text-lg font-bold">Total</p>
           <p className="text-sm md:text-lg font-bold">Rs. 0</p>
         </div>
-        <button className="w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600" onClick={() => handleCheckout()}>
+        <button className="w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600" onClick={()=> alert("you are not logged in and Cart is also empty")}>
           Check out
         </button>
       </div>
@@ -91,4 +97,4 @@ const Cart = () => {
   )
 }
 
-export default Cart
+export default Cart;
